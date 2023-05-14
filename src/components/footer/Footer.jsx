421e2dafa158bfd22ef266020a2944c8e1ea9ad7
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-05-13 19:06:25 -0500

    deploying react app to gh-pages

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <div className="Footer_">
+      <span>
+        Powered by <a href="top">w3.css</a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
